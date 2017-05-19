572a0cb331561f2427c458f32aa45c6979d37e15
jeffreymu <mujm@msn.com>
2017-05-19 14:32:57 +0800

    T1

---
+++ src/main/java/com/leting/microservice/admin/T1.java
@@ -0,0 +1,18 @@
+package com.leting.microservice.admin;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Jeffrey
+ * @created 2017-05-19 14:31
+ **/
+public class T1 {
+
+    public T1(){
+
+    }
+
+    public void test1() {
+        System.out.println("this is a develop branch");
+    }
+}
